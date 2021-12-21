ce3a1d4430464a393036d212d69baa3df7c6ffad
jakeroseboro <57158408+jakeroseboro@users.noreply.github.com>
2021-12-21 11:46:50 -0600

    config API call to make sure it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [pred, setPred] = useState(0)
+  useEffect(()=> {
+    val()
+    console.log(pred)
+  },[])
+
+  const getData = async () => {
+    var config = { headers: {  
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'}
+    }
+    const data = {
+      "Age": 60,
+      "Sex": 0,
+      "ChestPainType":1,
+      "RestingBP": 190,
+      "FastingBS": 1,
+      "RestingECG": 1,
+      "MaxHR": 100,
+      "ExerciseAngina":1,
+      "Oldpeak":2.0,
+      "ST_Slope":1
+  }
+    const res = await axios.post('https://heart-disease-ml-api.herokuapp.com/prediction', data, config)
+    return res.data
+  }
+  
+  const val = async() => {
+    const data = await getData()
+    setPred(data)
+  }
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +49,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          {pred}
         </a>
       </header>
     </div>
