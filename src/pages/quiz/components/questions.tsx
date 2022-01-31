11124264f0cd39a8cc8ee4a9e58bdb946423a3f3
jakeroseboro <57158408+jakeroseboro@users.noreply.github.com>
2022-01-31 16:52:38 -0600

    Update questions.tsx

--- src/pages/quiz/components/questions.tsx
+++ src/pages/quiz/components/questions.tsx
@@ -22,37 +22,58 @@ export const Questions = () =>{
                 </Form.Item>
                 <Form.Item hidden={quizNumber !== 1} label="Sex" name="sex" rules={[{required:true}]}>
                     <Select defaultValue={1}>
+                        <Option key ={0} value={0}>Female</Option>
                         <Option key ={1} value={1}>Male</Option>
                     </Select>
                 </Form.Item>
-                <Form.Item hidden={quizNumber !== 2}>
-
+                <Form.Item hidden={quizNumber !== 2} label="Chest Pain Type" name="ChestPainType" rules={[{required:true}]}>
+                    <Select defaultValue={0}>
+                        <Option key ={0} value={0}>Typical Angina</Option>
+                        <Option key ={1} value={1}>Atypical Angina</Option>
+                        <Option key ={2} value={2}>Non-Anginal Pain</Option>
+                        <Option key ={3} value={3}>Asymptomatic</Option>
+                    </Select>
                 </Form.Item>
-                <Form.Item hidden={quizNumber !== 3}>
-
+                <Form.Item hidden={quizNumber !== 3} label="Resting Blood Pressure" name="RestingBP" rules={[{required:true}]}>
+                    <Input/>
                 </Form.Item>
-                <Form.Item hidden={quizNumber !== 4}>
-
+                <Form.Item hidden={quizNumber !== 4} label="Fasting Blood Sugar" name="FastingBS" rules={[{required:true}]}>
+                    <Select defaultValue={0}>
+                        <Option key ={0} value={0}>No</Option>
+                        <Option key ={1} value={1}>Yes</Option>
+                    </Select>
                 </Form.Item>
-                <Form.Item hidden={quizNumber !== 5}>
-
+                <Form.Item hidden={quizNumber !== 5} label="Resting ECG" name="RestingECG" rules={[{required:true}]}>
+                    <Select defaultValue={0}>
+                        <Option key ={0} value={0}>Normal</Option>
+                        <Option key ={1} value={1}>ST: having ST-T wave abnormality</Option>
+                        <Option key ={2} value={2}>LVH: showing probable or definite left ventricular hypertrophy by Estes' criteria</Option>
+                    </Select>
                 </Form.Item>
-                <Form.Item hidden={quizNumber !== 6}>
-
+                <Form.Item hidden={quizNumber !== 6} label="Max Heart Rate" name="MaxHR" rules={[{required:true}]}>
+                    <Input/>
                 </Form.Item>
-                <Form.Item hidden={quizNumber !== 7}>
-
+                <Form.Item hidden={quizNumber !== 7} label="Exercise Angina" name="ExerciseAngina" rules={[{required:true}]}>
+                    <Select defaultValue={0}>
+                        <Option key ={1} value={1}>No</Option>
+                        <Option key ={0} value={0}>Yes</Option>
+                    </Select>
                 </Form.Item>
-                <Form.Item hidden={quizNumber !== 8}>
-
+                <Form.Item hidden={quizNumber !== 8} label="Old Peak" name="Oldpeak" rules={[{required:true}]}>
+                    <Input/>
                 </Form.Item>
-                <Form.Item hidden={quizNumber !== 9}>
-
+                <Form.Item hidden={quizNumber !== 9} label="ST Slope" name="ST_Slope" rules={[{required:true}]}>
+                    <Select defaultValue={0}>
+                        <Option key ={0} value={0}>Up</Option>
+                        <Option key ={1} value={1}>Flat</Option>
+                        <Option key ={2} value={2}>Down</Option>
+                    </Select>
                 </Form.Item>
-                <Form.Item hidden={quizNumber !== 10}>
-
+                <Form.Item hidden={quizNumber !== 10} label="Cholesterol" name="Cholesterol" rules={[{required:true}]}>
+                    <Input/>
                 </Form.Item>
                 <Form.Item>
+                    <Button type="primary" htmlType="submit" style={{margin: 15}}>Submit</Button>
                     <Button hidden={quizNumber === 10} onClick={() => setQuizNumber(quizNumber + 1)}>Next</Button>
                 </Form.Item>
             </Form>
